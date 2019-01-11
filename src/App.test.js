64094dc378ef0d55615fab2545bbfd0a357e9ab1
Charles Hoffman <choffman@pillartechnology.com>
2019-01-11 15:56:07 -0600

    Test for presence of an input

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { shallow } from 'enzyme';
+import App from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
+it("renders without crashing", () => {
+  const div = document.createElement("div");
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+describe("FizzBuzz app", () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<App />);
+  });
+
+  it("has a place for input", () => {
+    expect(wrapper.exists('#input')).toBe(true);
+  });
 });
--- src/__tests__/features/step_definitions/FizzBuzz.steps.js
+++ src/__tests__/features/step_definitions/FizzBuzz.steps.js
@@ -1,4 +1,7 @@
 import { defineFeature, loadFeature } from 'jest-cucumber';
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from '../../../App.js'
 
 const feature = loadFeature('./src/__tests__/features/FizzBuzz.feature');
 
@@ -8,11 +11,11 @@ defineFeature(feature, test => {
         let wrapper;
 
         beforeEach(() => {
-            
+            wrapper = shallow(<App />);
         });
 
         given('Input contains 17', () => {
-
+            wrapper.find('#input').simulate('change', { target: { value: '17' } });
         });
 
         when('I click submit', () => {
